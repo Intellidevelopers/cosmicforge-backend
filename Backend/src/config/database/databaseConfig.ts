ed2bfc2a713d1c90eb74c850816e9b16fcdecca0
Devjoe <benagu477@gmail.com>
2025-01-25 08:48:40 +0100

    fixed mongod db error

--- Backend/src/config/database/databaseConfig.ts
+++ Backend/src/config/database/databaseConfig.ts
@@ -1,19 +1,12 @@
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+import mongoose from 'mongoose'
 
  let  LOCAL_DB_URL = "mongodb://localhost:27017/cosmicbacken"
- let  STAGING_URL = "mongodb+srv://CosmicForge:cosmicForgeDb@23@cluster0.amtdi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+ let  STAGING_URL = "mongodb+srv://CosmicForge:DevTeam1234@cluster0.amtdi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 
-const uri = "mongodb+srv://CosmicForge:DevTeam1234@cluster0.amtdi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+
  export  const connectDB = async () =>{
-   return client.connect()
+   return mongoose.connect(STAGING_URL)
  } 
