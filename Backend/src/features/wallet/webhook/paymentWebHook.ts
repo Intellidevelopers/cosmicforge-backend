a2f1c5e634492124499eec5dbebe3e5584116131
Devjoe <benagu477@gmail.com>
2025-04-16 22:53:28 +0100

    update on code

--- Backend/src/features/wallet/webhook/paymentWebHook.ts
+++ Backend/src/features/wallet/webhook/paymentWebHook.ts
@@ -37,9 +37,11 @@ export const confirmPaymentAndSettleAccount =  async (req:TypedRequest<{
     if(event && event === "charge.success"){
 
      const  appointment = await BookAppointmentModel.findOne({
-       " payment.paymentReference":data.reference
+       "payment.paymentReference":data.reference
 
      })
+
+     console.log(appointment)
 
      if(appointment){
 
