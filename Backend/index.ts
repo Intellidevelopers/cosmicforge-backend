ba6875ec6602f5b1c30c9f6348580b1a73d53884
kacy-dev <kelechukwuvictor414@gmail.com>
2025-05-01 15:22:40 +0100

    Add admin endpoint

--- Backend/index.ts
+++ Backend/index.ts
@@ -46,7 +46,7 @@ app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({ extended: true,limit:'50mb' }));
 
 app.use('/api/v1/cosmicforge/',mainRouter)
-app.use('/api/auth/v1/cosmicforge/admin', adminAuthRoute)
+app.use('/api/v1/cosmicforge/auth/admin', adminAuthRoute)
 
 
 
