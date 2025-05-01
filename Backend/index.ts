c0db4abf54631247a2c4d64c5f26b4780430d5c8
Devjoe <benagu477@gmail.com>
2025-05-01 08:49:17 +0100

    update on code

--- Backend/index.ts
+++ Backend/index.ts
@@ -41,20 +41,21 @@ import { withdrawBallance } from './src/features/wallet/controller/walletControl
 
 app.set("view engine",'ejs')
 app.set('views','./src/views')
-// app.use(cors())
+ app.use(cors())
 const allowedOrigins = [
     "http://127.0.0.1:5500",
     "https://www.cfhealthnet.com",
     "http://localhost:5173"
   ];
   
-  app.use(
+  /*app.use(
     cors({
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
       origin: allowedOrigins,
     })
-);
+);*/
+
 app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({ extended: true,limit:'50mb' }));
 
